e43fa7df8db24dc16c8f86e8f002aacd3b3c57ba
guodongxiaren <879231132@qq.com>
2014-10-13 10:43:07 +0800

    toString convert to print method

--- Java/src/shiyan3/Complex.java
+++ Java/src/shiyan3/Complex.java
@@ -32,8 +32,7 @@ public class Complex {
 		return tmp;
 	}
 	
-	@Override
-	public String toString() {
+	public String print() {
 		// TODO Auto-generated method stub
 		if(this.imaginary>0)
 			return this.real+"+"+this.imaginary+"i";
@@ -64,10 +63,10 @@ public class Complex {
 		Complex a = new Complex(1,2);
 		Complex b = new Complex(4,5);
 		Complex c = new Complex(2,3);
-		System.out.println(a+" 和"+b+"相加等于 "+ a.plus(b));
-		System.out.println(a+" 和"+c+"相减等于 "+a.minus(c));
-		System.out.println(a+" 和"+12+"相加等于 "+a.plus(12));
-		System.out.println(a+" 和"+10+"相减等于 "+a.minus(10));
+		System.out.println(a+" 和"+b+"相加等于 "+ a.plus(b).print());
+		System.out.println(a+" 和"+c+"相减等于 "+a.minus(c).print());
+		System.out.println(a+" 和"+12+"相加等于 "+a.plus(12).print());
+		System.out.println(a+" 和"+10+"相减等于 "+a.minus(10).print());
 	
 	}
 
