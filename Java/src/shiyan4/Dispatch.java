27730d2020bb8435a58083847f70dda9d83e430d
guodongxiaren <879231132@qq.com>
2014-10-13 10:05:55 +0800

    shiyan4

---
+++ Java/src/shiyan4/Dispatch.java
@@ -0,0 +1,22 @@
+package shiyan4;
+
+import java.io.*;
+
+class A {
+	void callme() {
+		System.out.println("Inside A's callme()method");
+	}
+}
+
+class B extends A {
+	void callme() {
+		System.out.println("Inside B's callme() Method");
+	}
+}
+
+public class Dispatch {
+	public static void main(String args[]) {
+		A a = new B();
+		a.callme();
+	}
+}
---
+++ Java/src/shiyan4/Inheritance.java
@@ -0,0 +1,39 @@
+package shiyan4;
+
+import java.io.*;
+
+class SuperClass {
+	int x;
+
+	SuperClass() {
+		x = 5;
+		System.out.println("in SuperClass : x=" + x);
+	}
+
+	void doSomething() {
+		System.out.println("in SuperClass.doSomething()");
+	}
+}
+
+class SubClass extends SuperClass {
+	int x;
+
+	SubClass() {
+		super(); // 调用父类的构造方法
+		x = 5; // super( ) 要放在方法中的第一句
+		System.out.println("in SubClass :x=" + x);
+	}
+
+	void doSomething() {
+		super.doSomething(); // 调用父类的方法
+		System.out.println("in SubClass.doSomething()");
+		System.out.println("super.x=" + super.x + " sub.x=" + x);
+	}
+}
+
+public class Inheritance {
+	public static void main(String args[]) {
+		SubClass subC = new SubClass();
+		subC.doSomething();
+	}
+}
