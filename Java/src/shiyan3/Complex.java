a79d4bed95f1458ea24ed1330c0b59b83ca6c06d
guodongxiaren <879231132@qq.com>
2014-10-20 12:16:03 +0800

    Comlex修改

--- Java/src/shiyan3/Complex.java
+++ Java/src/shiyan3/Complex.java
@@ -63,10 +63,12 @@ public class Complex {
 		Complex a = new Complex(1,2);
 		Complex b = new Complex(4,5);
 		Complex c = new Complex(2,3);
-		System.out.println(a+" 和"+b+"相加等于 "+ a.plus(b).print());
-		System.out.println(a+" 和"+c+"相减等于 "+a.minus(c).print());
-		System.out.println(a+" 和"+12+"相加等于 "+a.plus(12).print());
-		System.out.println(a+" 和"+10+"相减等于 "+a.minus(10).print());
+		c=a.plus(b);
+		System.out.print(c.print());
+		System.out.println(a.print()+" 和"+b.print()+"相加等于 "+ a.plus(b).print());
+		System.out.println(a.print()+" 和"+c.print()+"相减等于 "+a.minus(c).print());
+		System.out.println(a.print()+" 和"+12+"相加等于 "+a.plus(12).print());
+		System.out.println(a.print()+" 和"+10+"相减等于 "+a.minus(10).print());
 	
 	}
 
