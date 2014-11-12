9537ed5c9f89f6e97bed6f04afe0c43c07f77ce3
guodongxiaren <879231132@qq.com>
2014-11-12 23:53:18 +0800

    求校验码的函数完成 坑爹的老师，实验材料里给的计算权重的算法是错误的。 和网上的完全不同。我纠结了半天，到底选哪一个。最后经过测试，老师的算法的确有误。

--- Java/src/shiyan6/idcard/Check.java
+++ Java/src/shiyan6/idcard/Check.java
@@ -3,8 +3,12 @@ package shiyan6.idcard;
 public class Check {
 	private String code;
 	private String error;
-	private static int days[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30,
-			31 };
+	// 每月天数。二月在下面特殊计算
+	private static int days[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30,31 };
+	// 网上找的身份证前17位的权值
+	private static int weight[] = { 7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5,8, 4, 2, 1 };
+	//每一位数字所对应的校验码
+	private static char chkcode[] = { '1', '0', 'X', '9', '8', '7', '6', '5','4', '3', '2' };
 	private int year;
 	private int mon;
 	private int day;
@@ -31,7 +35,7 @@ public class Check {
 	}
 
 	public static void main(String args[]) {
-		Check c = new Check("440524198001010013");
+		Check c = new Check("130922199310173618");
 		c.isTrue();
 	}
 
@@ -50,17 +54,23 @@ public class Check {
 		System.out.println(qy.getAddress());
 		// sub截取身份证号的生日部分
 		sub = old ? code.substring(6, 12) : code.substring(6, 14);
-
-		if (!trueBirth(sub))
+		if (!trueBirth(sub)) {
+			error = "出生日期错误";
 			return false;
+		}
 		System.out.println(year + "年" + mon + "月" + day + "日");
+		if (checkCode() == code.charAt(17)) {
+			error = "校验码有误";
+			return false;
+		}
+
 		return true;
 	}
 
 	/**
 	 * 判断生日的真假
 	 */
-	public boolean trueBirth(String birthday) {
+	private boolean trueBirth(String birthday) {
 		String tmp;
 		if (old == true) {
 			tmp = "19" + birthday.substring(0, 2);
@@ -71,21 +81,32 @@ public class Check {
 			year = Integer.parseInt(tmp);
 			tmp = birthday.substring(4);
 		}
-
 		mon = Integer.parseInt(tmp.substring(0, 2));
 		if (mon > 12)
 			return false;
 		day = Integer.parseInt(tmp.substring(2));
-
 		int run = 0;// 是不是闰年，用整型来表示，方便下面作加法
 		if (year % 4 == 0 && year % 400 != 0)
 			run = 1;
 		if (mon == 2) {
 			if (day > days[2] + run)
 				return false;
-		} else if (day > days[mon-1])// 如果不在该月天数的范围内，则为假生日
+		} else if (day > days[mon - 1])// 如果不在该月天数的范围内，则为假生日
 			return false;
-
 		return true;
+	}
+
+	/*
+	 * 求校验码
+	 */
+	private char checkCode() {
+		int sum = 0;
+		for (int i = 0; i < 17; i++) {
+			int a = code.charAt(i) - '0';
+			int w = weight[i];
+			sum += a * w;
+		}
+		sum %= 11;
+		return chkcode[sum];
 	}
 }
