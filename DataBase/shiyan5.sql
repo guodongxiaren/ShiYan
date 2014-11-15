808be56066a513f618a233f63a266b4a41d5488b
guodongxiaren <879231132@qq.com>
2014-11-15 19:43:58 +0800

    shiyan5 sql

--- DataBase/shiyan5.sql
+++ DataBase/shiyan5.sql
@@ -1,15 +1,42 @@
-use market
-go
-create proc proc_hub 
-as
-begin
-declare @orderid int,@goodsid int,@customerid int
-select @goodsid=goodsid from goods where goodsname='鸽鸽'
-select @orderid=orderid from orders where goodsid=@goodsid
-select @customerid=customerid from orders where orderid=@orderid
-select @orderid,customername from customer where @customerid=customerid
-end
+USE market 
 
-exec proc_hub
--- 删除存储过程
-drop proc proc_hub
+go 
+
+CREATE PROC proc_hub 
+AS 
+  BEGIN 
+      SELECT orderid      '订单号', 
+             customername '客户姓名', 
+             quantity     '订货数量' 
+      FROM   orders, 
+             customer 
+      WHERE  orders.customerid = customer.customerid 
+             AND orderid IN (SELECT orderid 
+                             FROM   orders 
+                             WHERE  goodsid = (SELECT goodsid 
+                                               FROM   goods 
+                                               WHERE  goodsname = 'hub')); 
+  END 
+
+-- 执行存储过程proc_hub 
+EXEC proc_hub 
+
+-- 创建带参数的存储过程proc_goods 
+go 
+
+CREATE PROC proc_goods @goodsname VARCHAR(20) 
+AS 
+  BEGIN 
+      SELECT * 
+      FROM   orders 
+      WHERE  goodsid = (SELECT goodsid 
+                        FROM   goods 
+                        WHERE  goodsname = @goodsname); 
+  END 
+
+-- 执行proc_goods 
+EXEC proc_goods 'hub' 
+
+-- 删除存储过程 
+DROP PROC proc_hub 
+DROP PROC proc_goods
