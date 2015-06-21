3b4ac5d78179f986c756c1456bb83d39e588c48f
guodongxiaren <879231132@qq.com>
2015-06-21 18:13:51 +0800

    -O2误一生 本来直接用gcc编译，程序运行完全没有问题。后来在Makefile里面使用了-O2进行优化，结果再次运行的时候出现了问题。 而因为代码是一个多月之前的了，我忘记了我改过Makefile。所以调试了好久。。最后才发现这个-O2。。 这个故事告诉我们，不是所有程序都适合优化。还告诉我们过分依赖Makefile可能会忘记问题所在。 还告诉我们实验报告一定要及时写，隔了一个月采写，就忘记了自己最后改动了什么了。

--- Linux/test4/customer.c
+++ Linux/test4/customer.c
@@ -10,19 +10,23 @@
 const char *fifo_file = "./myfifo";
 const char *tmp_file = "./tmp";
 
-int customing(const char *myfifo,int need){
+int customing(const char *myfifo,int need)
+{
     int fd;
     char buff;
     int counter = 0;
-    if((fd = open(myfifo,O_RDONLY)) < 0){
+    if((fd = open(myfifo,O_RDONLY)) < 0)
+    {
         fprintf(stderr,"Function customing error\n");
         return -1;
     }
     printf("Enjoy:");
     // 使fd重置为文件起始位置
     lseek(fd,0,SEEK_SET);
-    while(counter < need){
-        while((read(fd,&buff,1) == 1)&&(counter < need)){
+    while(counter < need)
+    {
+        while((read(fd,&buff,1) == 1)&&(counter < need))
+        {
             fputc(buff,stdout);
             counter++;
         }
@@ -35,59 +39,66 @@ int customing(const char *myfifo,int need){
 /* myfilecopy()
  * 实现从sour_file 文件的offset偏移处将count个字节复制到dest_file文件
  */
- int myfilecopy(const char *sour_file,const char *dest_file,
+int myfilecopy(const char *sour_file,const char *dest_file,
                int offset,int count,int copy_mode)
- {
-     int in_file,out_file;
-     int counter = 0;
-     char buff_unit;
-     if((in_file = open(sour_file,O_RDONLY|O_NONBLOCK)) < 0){
-         fprintf(stderr,"Function myfilecopy error in source file\n");
-         return -1;
-     }
-     if((out_file = open(dest_file,O_CREAT|O_RDWR|O_NONBLOCK,0644)) < 0){
-         fprintf(stderr,"Function myfilecopy error in destination file\n");
-         return -1;
-     }
-     lseek(in_file,offset,SEEK_SET);
-     while((read(in_file,&buff_unit,1) == 1)&&(counter < count)){
-         if(write(out_file,&buff_unit,1)){
-             perror("write out_file failed");
-             exit(EXIT_FAILURE);
-         }
-         counter++;
-     }
-     close(in_file);
-     close(out_file);
-     return 0;
- }
+{
+    int in_file,out_file;
+    int counter = 0;
+    char buff_unit;
+    if((in_file = open(sour_file,O_RDONLY|O_NONBLOCK)) < 0)
+    {
+        fprintf(stderr,"Function myfilecopy error in source file\n");
+        return -1;
+    }
+    if((out_file = open(dest_file,O_CREAT|O_RDWR|O_NONBLOCK,0644)) < 0)
+    {
+        fprintf(stderr,"Function myfilecopy error in destination file\n");
+        return -1;
+    }
+    lseek(in_file,offset,SEEK_SET);
+    while((read(in_file,&buff_unit,1) == 1)&&(counter < count))
+    {
+        if(write(out_file,&buff_unit,1)){
+            perror("write out_file failed");
+            exit(EXIT_FAILURE);
+        }
+        counter++;
+    }
+    close(in_file);
+    close(out_file);
+    return 0;
+}
 
  /*custom()
   * 实现FIFO消费者
   */
-  int custom(int need){
-      int fd;
-      customing(fifo_file,need);
-      if((fd = open(fifo_file,O_RDWR)) < 0){
-         fprintf(stderr,"Function custom error in source file\n");
-         return -1;
-      }
-      lock_set(fd,F_WRLCK);
-      myfilecopy(fifo_file,tmp_file,need,MAX_FILE_SIZE,0);
-      myfilecopy(tmp_file,fifo_file,0,MAX_FILE_SIZE,0);
-      lock_set(fd,F_UNLCK);
-      unlink(tmp_file);
-      close(fd);
-      return 0;
-  }
+int custom(int need)
+{
+    int fd;
+    customing(fifo_file,need);
+    if((fd = open(fifo_file,O_RDWR)) < 0)
+    {
+        fprintf(stderr,"Function custom error in source file\n");
+        return -1;
+    }
+    lock_set(fd,F_WRLCK);
+    myfilecopy(fifo_file,tmp_file,need,MAX_FILE_SIZE,0);
+    myfilecopy(tmp_file,fifo_file,0,MAX_FILE_SIZE,0);
+    lock_set(fd,F_UNLCK);
+    unlink(tmp_file);
+    close(fd);
+    return 0;
+}
 
-  int main(int argc,char **argv){
-      int customer_capacity = 10;
-      if(argc > 1){
-          sscanf(argv[1],"%d",&customer_capacity);
-      }
-      if(customer_capacity > 0){
-          custom(customer_capacity);
-      }
-      exit(EXIT_SUCCESS);
-  }
+int main(int argc, char **argv)
+{
+    int customer_capacity = 10;
+    if(argc > 1){
+        sscanf(argv[1],"% d",&customer_capacity);
+    }
+    if(customer_capacity > 0)
+    {
+        custom(customer_capacity);
+    }
+    exit(EXIT_SUCCESS);
+}
--- Linux/test4/producer.c
+++ Linux/test4/producer.c
@@ -13,35 +13,39 @@
 #define DIGIT_START '0'
 #define COUNT_OF_DIGIT 10
 #define SIGN_TYPE ALPHABET
+
 const char *fifo_file = "./myfifo";
 char buff[MAXLEN];
 
-int product(void){
+int product(void)
+{
     int fd;
     unsigned int sign_type,sign_start,sign_count,size;
     static unsigned int counter = 0;
 
-    if((fd = open(fifo_file,O_CREAT|O_RDWR|O_APPEND,0644)) < 0){
+    if((fd = open(fifo_file, O_CREAT|O_RDWR|O_APPEND, 0644)) < 0){
         printf("Open fifo file error\n");
         exit(1);
     }
     sign_type = SIGN_TYPE;
-    switch(sign_type){
+    switch(sign_type)
+    {
         case ALPHABET:
-                      sign_start = ALPHABET_START;
-                      sign_count = COUNT_OF_ALPHABET;
-                      break;
+            sign_start = ALPHABET_START;
+            sign_count = COUNT_OF_ALPHABET;
+        break;
         case DIGIT:
-                      sign_start = DIGIT_START;
-                      sign_count = COUNT_OF_DIGIT;
-                      break;
+            sign_start = DIGIT_START;
+            sign_count = COUNT_OF_DIGIT;
+        break;
         default:
-                      return -1;
+        return -1;
     }
-    sprintf(buff,"%c",(sign_start + counter));
+    sprintf(buff, "%c", (sign_start + counter));
     counter = (counter + 1) % sign_count;
     lock_set(fd,F_WRLCK);
-    if((size = write(fd,buff,strlen(buff))) < 0){
+    if((size = write(fd,buff,strlen(buff))) < 0)
+    {
         printf("Producter:write error\n");
         return -1;
     }
@@ -50,16 +54,20 @@ int product(void){
     return 0;
 }
 
-int main(int argc,char **argv){
+int main(int argc, char **argv)
+{
     int time_step = 1;
     int time_life = 10;
-    if(argc > 1){
+    if(argc > 1)
+    {
         sscanf(argv[1],"%d",&time_step);
     }
-    if(argc > 2){
-        sscanf(argv[2],"%d",&time_step);
+    if(argc > 2)
+    {
+        sscanf(argv[2],"%d",&time_life);
     }
-    while(time_life--){
+    while(time_life--)
+    {
         if(product() < 0)
             break;
         sleep(time_step);
